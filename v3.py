48e78f9c452c820c671137e4d859f7320acb65fa
Methminpulsara <methminpulsara10@gmail.com>
2025-11-22 16:33:21 +0530

    update

---
+++ v3.py
@@ -0,0 +1,71 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+from models.vehicle import Car, CarResponse
+from fastapi import FastAPI
+
+load_dotenv()
+
+OPENROUTER_KEY = os.getenv("OPENROUTER_KEY")
+OPENROUTER_URL = os.getenv("OPENROUTER_URL")
+
+summerlize_llm = ChatOllama(
+
+    model="llama-3.2"
+)
+
+summarizer_promt = ChatPromptTemplate.from_template(
+    """Extract car price in rupees, manufacture year, and mileage for the given car model:
+                {car_model}
+
+                Using this information:
+                {docs}
+            """
+)
+
+summarize_chain = summarizer_promt | summerlize_llm
+
+response_pormt = ChatPromptTemplate.from_template(
+    """
+You are an automotive assistance helping users find the best car deals.
+Here is a summarized list of car ads for {car_model}.
+
+{summary}
+
+Highlight key insight, provide a 2-3 summary , also provide best average price to buy
+""")
+
+anaylize_llm = ChatOllama(
+    model="llama-3.2"
+).with_structured_output(CarResponse)
+
+analysis_chain = response_pormt | anaylize_llm
+
+app = FastAPI()
+
+
+@app.get("/car-ads/{car_model}", response_model=CarResponse)
+def car_analysis(car_model: str):
+    car_model = "axio"
+
+    urls = [
+        f"https://ikman.lk/en/ads?query={car_model}",
+        f"https://patpat.lk/en/sri-lanaka/vehicle/car/toyota/{car_model}"
+    ]
+    website_loader = WebBaseLoader(urls)
+    docs = website_loader.load()
+
+    ads_summary = summarize_chain.invoke({
+        "car_model": car_model,
+        "docs": "\n".join([doc.page_content for doc in docs])
+    })
+
+    # Promt eke dena vearibels names match wenn one
+    final_output = analysis_chain.invoke({"car_model": car_model,
+                                          "summary": ads_summary
+                                          })
+
+    return final_output
+
